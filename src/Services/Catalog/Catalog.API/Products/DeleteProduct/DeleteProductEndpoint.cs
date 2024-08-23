db7b7204f7d6ed74a9dfa52ed758487e476f1448
Josh Gerlach <josh.w.gerlach@gmail.com>
2024-08-23 17:49:09 +1000

    feat: add DeleteProduct Endpoint and Handler

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id:guid}", async (Guid id, ISender sender)
+            =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+
+            return Results.NoContent();
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status204NoContent)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete a Product")
+        .WithDescription("Delete a Product");
+    }
+}
